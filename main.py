481f5fd375b08b3b7897eff7668a7b67b34d5fa3
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-26 01:43:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 # main.py
-
 from fastapi import FastAPI
 
+from pydantic import BaseModel
 
 app = FastAPI()
+# to see what funny will come
+app.counter = 0
+
+
+class HelloResp(BaseModel):
+    msg: str
 
 @app.get("/")
 def root():
-    return {"message": "Hello Wurld!!!"}
+    return {"message": "Hello World"}
 
 
-@pytest.mark.parametrize("name", ['Zenek', 'Marek', 'Alojzy Niezdąży'])
-def test_hello_name(name):
-    response = client.get(f"/hello/{name}")
-    assert response.status_code == 200
-    assert response.text == f'"Hello {name}"'
+@app.get('/counter')
+def counter():
+    app.counter += 1
+    return str(app.counter)
+
+
+@app.get("/hello/{name}", response_model=HelloResp)
+async def read_item(name: str):
+    return HelloResp(msg=f"Hello {name}")
