3271822590eaabededa034287a56815e22ff119a
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-26 01:45:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,3 +25,17 @@ def counter():
 @app.get("/hello/{name}", response_model=HelloResp)
 async def read_item(name: str):
     return HelloResp(msg=f"Hello {name}")
+
+
+class GiveMeSomethingRq(BaseModel):
+    first_key: str
+
+
+class GiveMeSomethingResp(BaseModel):
+    received: Dict
+    constant_data: str = "python jest super"
+
+
+@app.post("/dej/mi/coś", response_model=GiveMeSomethingResp)
+def receive_something(rq: GiveMeSomethingRq):
+    return GiveMeSomethingResp(received=rq.dict())
