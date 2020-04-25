32a1764e83fd34e079e266814f25c659aa0b8169
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-26 01:39:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ app = FastAPI()
 def root():
     return {"message": "Hello Wurld!!!"}
 
-@app.get("/hello/{name}")
-async def read_item(name: str):
-    return f"Hello {name}"
+
+@pytest.mark.parametrize("name", ['Zenek', 'Marek', 'Alojzy Niezdąży'])
+def test_hello_name(name):
+    response = client.get(f"/hello/{name}")
+    assert response.status_code == 200
+    assert response.text == f'"Hello {name}"'
