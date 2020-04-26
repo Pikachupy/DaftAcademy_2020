1bd808b10f3d8e5d5260b871905e3b91445b91aa
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-26 23:51:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,13 @@ from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi import FastAPI, Response, status
 from fastapi import Depends, Cookie, HTTPException
 from hashlib import sha256
-
 import secrets
-
 
 app = FastAPI()
 
-
-@app.get("/welcome")
+@app.get('/welcome')
 def get_welcome():
-	return "Hello!"
+	return {"message": "Hello World during the coronavirus pandemic!"}
 
 
 app.secret_key = "very constatn and random secret, best 64 characters"
@@ -20,34 +17,20 @@ app.tokens = []
 security = HTTPBasic()
 
 
-def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
-	correct_username = secrets.compare_digest(credentials.username, "trudnY")
-	correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
-	print(f"{credentials.username}")
-	print(f"{credentials.password}")
+def user(credentials: HTTPBasicCredentials = Depends(security)):
+	correct_username = secrets.compare_digest(credentials.username, 'trudnY')
+	correct_password = secrets.compare_digest(credentials.password, 'PaC13Nt')
 	if not (correct_username and correct_password):
-		raise HTTPException(
-		status_code=status.HTTP_401_UNAUTHORIZED,
-		)
+		raise HTTPException(status_code=401)
 	else:
-		session_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}", encoding='utf8')).hexdigest()
-		return session_token
-    
+		s_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}", encoding='utf8')).hexdigest()
+		return s_token
 
-
-
-@app.post("/login")
-def login(
-	response: Response,
-	session_token = Depends(get_current_username)
-	):
-    
-	app.tokens+=(session_token)
-    
-	response.set_cookie(key="session_token", value=session_token)
-    
-        
-	response = RedirectResponse(url = "/welcome")
+	
+@app.post('/login')
+def login(response: Response, s_token = Depends(user)):
+	app.tokens += s_token
+	response.set_cookie(key="s_token", value=s_token)
+	response = RedirectResponse(url = '/welcome')
 	response.status_code = status.HTTP_302_FOUND
-    
 	return response
