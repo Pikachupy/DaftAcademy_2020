6233c3ba9b1bf1fa9985682a7ad29da65d6a6f7a
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-26 18:31:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,6 @@ app.tokens = []
 patlist = []
 
 
-@app.get("/")
-def root():
-	return {"message": "Hello World during the coronavirus pandemic!"}
-
-
 @app.get("/welcome")
 def welcome_to_the_jungle():
 	return {"message": "Welcome to the jungle! We have funny games!"}
@@ -26,14 +21,15 @@ def welcome_to_the_jungle():
 @app.post("/login/")
 def login_to_app(user: str, passw: str, response: Response):
 	if user in app.users and passw == app.users[user]:
-		s_token = sha256(bytes(f"{user}{passw}{app.secret}", encoding='utf8')).hexdigest()
+		s_token = sha256(bytes(f"{user}{passw}{app.secret}")).hexdigest()
 		app.tokens += s_token
-		response.set_cookie(key="session_token",value=s_token)
+		response.set_cookie(key="s_token",value=s_token)
 		response = RedirectResponse(url='/welcome')
 		print('logged in')
+		response(status_code=status.HTTP_302_FOUND)
 		return response
 	else:
-		raise HTTPException(status_code=401)
+		raise HTTPException(status_code=403, detail="Unathorised")
 '''
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
     correct_username = secrets.compare_digest(credentials.username, "trudnY")
