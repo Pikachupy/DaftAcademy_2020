55c1148d0b887a138013f546d8a01b57956cea5f
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-26 17:10:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,105 +1,53 @@
-from typing import Dict
-
-from fastapi import FastAPI, Request, Response, status
-from pydantic import BaseModel
-from starlette.authentication import requires
-from starlette.authentication import (
-    AuthenticationBackend, AuthenticationError, SimpleUser, UnauthenticatedUser,
-    AuthCredentials
-)
-from starlette.middleware import Middleware
-from starlette.middleware.authentication import AuthenticationMiddleware
-from starlette.responses import PlainTextResponse
-from starlette.routing import Route
-import base64
-import binascii
-
-app = FastAPI()
-app.counter: int = 0
-app.storage: Dict[int, Patient] = {}
-app.users={"admin":"admin"}
-'''
-class BasicAuthBackend(AuthenticationBackend):
-    async def authenticate(self, request):
-        if "Authorization" not in request.headers:
-            return
-        auth = request.headers["Authorization"]
-        try:
-            scheme, credentials = auth.split()
-            if scheme.lower() != 'basic':
-                return
-            decoded = base64.b64decode(credentials).decode("ascii")
-        except (ValueError, UnicodeDecodeError, binascii.Error) as exc:
-            raise AuthenticationError('Invalid basic auth credentials')
-
-        username, _, password = decoded.partition(":")
-        # TODO: You'd want to verify the username and password here.
-        if username in app.users and password == app.users[username]:
-            return AuthCredentials(["authenticated"]), SimpleUser(username)
-        else:
-            raise HTTPException(status_code=403, detail="Unathorised")
-            
-
-@app.get("/")
-async def homepage(request):
-    if request.user.is_authenticated:
-        return PlainTextResponse('Hello, ' + request.user.display_name)
-    return PlainTextResponse('Hello, you')
-'''
-
-
+from starlette.responses import RedirectResponse
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi import FastAPI, Response, status
+from fastapi import Depends, Cookie, HTTPException
 from hashlib import sha256
-from fastapi import FastAPI, Response, Cookie, HTTPException
+
+import secrets
+
 
 app = FastAPI()
-app.secret_key = "very constatn and random secret, best 64 characters"
 
 
-@app.post("/login/")
-def create_cookie(user: str, password: str, response: Response):
-    session_token = sha256(bytes(f"{user}{password}{app.secret_key}")).hexdigest()
-    response.set_cookie(key="session_token", value=session_token)
-    return  AuthCredentials(["authenticated"]),{"message": "Welcome"}
-
-@app.get("/data/")
-def create_cookie(*, response: Response, session_token: str = Cookie(None)):
-    if session_token not in Database :
-        raise HTTPException(status_code=403, detail="Unathorised")
-    response.set_cookie(key="session_token", value=session_token)
-    
-    
-class Patient(BaseModel):
-    name: str
-    surename: str
-
-
-
-@requires(['authenticated'], status_code=404)
-@app.get("/")
-def read_root():
-    return {"message": "Hello World during the coronavirus pandemic!"}
-
-@requires(['authenticated'], status_code=404)
 @app.get("/welcome")
-def read_root():
-    return {"message": "Hey Hi Hello!"}
+def get_welcome():
+	return "Hello!"
 
-@requires(['authenticated'], status_code=404)
-@app.api_route(path="/method", methods=["GET", "POST", "DELETE", "PUT", "OPTIONS"])
-def read_request(request: Request):
-    return {"method": request.method}
 
-@requires(['authenticated'], status_code=404)
-@app.post("/patient")
-def show_data(patient: Patient):
-    resp = {"id": app.counter, "patient": patient}
-    app.storage[app.counter] = patient
-    app.counter += 1
-    return resp
+app.secret_key = "very constatn and random secret, best 64 characters"
+app.tokens_list = []
+security = HTTPBasic()
 
-@requires(['authenticated'], status_code=404)
-@app.get("/patient/{pk}")
-def show_patient(pk: int):
-    if pk in app.storage:
-        return app.storage.get(pk)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "trudnY")
+    correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
+    print(f"{credentials.username}")
+    print(f"{credentials.password}")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            # detail="Incorrect email or password",
+            # headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+
+
+@app.post("/login")
+def login(
+    user: str, password: str, response: Response,
+    credentials_user = Depends(get_current_username)
+    ):
+    
+    session_token = sha256(bytes(f"{user}{password}{app.secret_key}", encoding='utf8')).hexdigest()
+    app.tokens_list.append(session_token)
+    
+    response.set_cookie(key="session_token", value=session_token)
+    
+        
+    response = RedirectResponse(url = "/welcome")
+    response.status_code = status.HTTP_302_FOUND
+    
+    return response
