4e57444dfbb336de30b7258d75126d85b359af21
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-26 02:02:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,39 @@
 from typing import Dict
 
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request, Response, status
 from pydantic import BaseModel
 
 
+class Patient(BaseModel):
+    name: str
+    surename: str
+
+
 app = FastAPI()
+app.counter: int = 0
+app.storage: Dict[int, Patient] = {}
 
 
 @app.get("/")
-def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"message": "Hello World during the coronavirus pandemic!"}
 
 
-class HelloResp(BaseModel):
-    msg: str
+@app.api_route(path="/method", methods=["GET", "POST", "DELETE", "PUT", "OPTIONS"])
+def read_request(request: Request):
+    return {"method": request.method}
 
 
-@app.get("/hello/{name}", response_model=HelloResp)
-def read_item(name: str):
-    return HelloResp(msg=f"Hello {name}")
+@app.post("/patient")
+def show_data(patient: Patient):
+    resp = {"id": app.counter, "patient": patient}
+    app.storage[app.counter] = patient
+    app.counter += 1
+    return resp
 
 
-class GiveMeSomethingRq(BaseModel):
-    first_key: str
-
-
-class GiveMeSomethingResp(BaseModel):
-    received: Dict
-    constant_data: str = "python jest super"
-
-
-@app.post("/dej/mi/coś", response_model=GiveMeSomethingResp)
-def receive_something(rq: GiveMeSomethingRq):
-    return GiveMeSomethingResp(received=rq.dict())
+@app.get("/patient/{pk}")
+def show_patient(pk: int):
+    if pk in app.storage:
+        return app.storage.get(pk)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
