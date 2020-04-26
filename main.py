9d8e8893cc6c4fd699249c4a7a22a355d9862341
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-26 16:21:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,42 @@
-from starlette.authentication import (
-    AuthenticationBackend, AuthenticationError, SimpleUser, UnauthenticatedUser,
-    AuthCredentials
-)
-from starlette.middleware import Middleware
-from starlette.middleware.authentication import AuthenticationMiddleware
-from starlette.responses import PlainTextResponse
-from starlette.routing import Route
-import base64
-import binascii
+from typing import Dict
+
+from fastapi import FastAPI, Request, Response, status
+from pydantic import BaseModel
 
 
-class BasicAuthBackend(AuthenticationBackend):
-    async def authenticate(self, request):
-        if "Authorization" not in request.headers:
-            return
-
-        auth = request.headers["Authorization"]
-        try:
-            scheme, credentials = auth.split()
-            if scheme.lower() != 'basic':
-                return
-            decoded = base64.b64decode(credentials).decode("ascii")
-        except (ValueError, UnicodeDecodeError, binascii.Error) as exc:
-            raise AuthenticationError('Invalid basic auth credentials')
-
-        username, _, password = decoded.partition(":")
-        # TODO: You'd want to verify the username and password here.
-        return AuthCredentials(["authenticated"]), SimpleUser(username)
+class Patient(BaseModel):
+    name: str
+    surename: str
 
 
-async def homepage(request):
-    if request.user.is_authenticated:
-        return PlainTextResponse('Hello, ' + request.user.display_name)
-    return PlainTextResponse('Hello, you')
+app = FastAPI()
+app.counter: int = 0
+app.storage: Dict[int, Patient] = {}
 
-routes = [
-    Route("/", endpoint=homepage)
-]
 
-middleware = [
-    Middleware(AuthenticationMiddleware, backend=BasicAuthBackend())
-]
+@app.get("/")
+def read_root():
+    return {"message": "Hello World during the coronavirus pandemic!"}
 
-app = Starlette(routes=routes, middleware=middleware)
+@app.get("/welcome")
+def read_root():
+    return {"message": "Hey Hi Hello!"}
+
+@app.api_route(path="/method", methods=["GET", "POST", "DELETE", "PUT", "OPTIONS"])
+def read_request(request: Request):
+    return {"method": request.method}
+
+
+@app.post("/patient")
+def show_data(patient: Patient):
+    resp = {"id": app.counter, "patient": patient}
+    app.storage[app.counter] = patient
+    app.counter += 1
+    return resp
+
+
+@app.get("/patient/{pk}")
+def show_patient(pk: int):
+    if pk in app.storage:
+        return app.storage.get(pk)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
