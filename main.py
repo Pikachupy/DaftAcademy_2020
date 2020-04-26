f45590ec339a57ba2cc73c8e865172ce0b4fae00
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-26 23:43:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,35 +21,33 @@ security = HTTPBasic()
 
 
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, "trudnY")
-    correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
-    print(f"{credentials.username}")
-    print(f"{credentials.password}")
-    if not (correct_username and correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            # detail="Incorrect email or password",
-            # headers={"WWW-Authenticate": "Basic"},
-        )
-    else:
-	session_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}", encoding='utf8')).hexdigest()
-	return session_token
+	correct_username = secrets.compare_digest(credentials.username, "trudnY")
+	correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
+	print(f"{credentials.username}")
+	print(f"{credentials.password}")
+	if not (correct_username and correct_password):
+		raise HTTPException(
+		status_code=status.HTTP_401_UNAUTHORIZED,
+		)
+	else:
+		session_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}", encoding='utf8')).hexdigest()
+		return session_token
     
 
 
 
 @app.post("/login")
 def login(
-    response: Response,
-    session_token = Depends(get_current_username)
-    ):
+	response: Response,
+	session_token = Depends(get_current_username)
+	):
     
-    app.tokens+=(session_token)
+	app.tokens+=(session_token)
     
-    response.set_cookie(key="session_token", value=session_token)
+	response.set_cookie(key="session_token", value=session_token)
     
         
-    response = RedirectResponse(url = "/welcome")
-    response.status_code = status.HTTP_302_FOUND
+	response = RedirectResponse(url = "/welcome")
+	response.status_code = status.HTTP_302_FOUND
     
-    return response
+	return response
