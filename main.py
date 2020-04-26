3351bedc60189d5526a97d92a29138761dcf774b
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-26 17:38:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -48,6 +48,6 @@ def login(
     
         
     response = RedirectResponse(url = "/welcome")
-    response.status_code = 302
+    response(status_code=status.HTTP_302_FOUND)
     
     return response
