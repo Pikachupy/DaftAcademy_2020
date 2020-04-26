9458109d9cbce4dd5d3678c839f66a1ad5dd5e7b
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-26 23:08:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,33 +18,6 @@ def get_welcome():
 app.secret_key = "very constatn and random secret, best 64 characters"
 app.tokens = []
 
-@app.get('/login')
-@app.post("/login/")
-def create_cookie(user: str, password: str, response: Response):
-	try :
-		if user == 'trudnY' and password == 'PaC13Nt':
-			return  {"message": "Welcome1"}
-			session_token = sha256(bytes(f"{user}{password}{app.secret_key}")).hexdigest()
-			app.tokens+=session_token
-			response = RedirectResponse(url='/welcome')
-			response.set_cookie(key="session_token", value=session_token)
-			response.status_code=302
-			return response
-		else:
-			return  {"message": "Welcome2"}
-			raise AuthenticationError('Invalid basic auth credentials')
-	except KeyError: 
-		return  {"message": "Welcome3"}
-		raise HTTPException(401,"User does not exists")
-		
-'''
-@app.get("/data/")
-def create_cookie(*, response: Response, session_token: str = Cookie(None)):
-    if session_token not in app.tokens :
-        raise HTTPException(status_code=403, detail="Unathorised")
-    response.set_cookie(key="session_token", value=session_token)
-
-security = HTTPBasic()
 
 
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
@@ -71,9 +44,11 @@ def login(
     session_token = sha256(bytes(f"{user}{password}{app.secret_key}", encoding='utf8')).hexdigest()
     app.tokens_list += session_token
     
-    response.set_cookie(key="session_token", value=session_token)
+   
     
-    response= requests.post('https://pikachupy.herokuapp.com/welcome', allow_redirects=False)
+    response= RedirectResponse(url='/welcome')
+    response.set_cookie(key="session_token", value=session_token)
+    response.status_code=302
     
     return response
-'''
+
