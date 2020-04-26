67edb90de88039696570903eab681c39929e8f5d
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-26 22:16:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,56 @@ def get_welcome():
 
 
 app.secret_key = "very constatn and random secret, best 64 characters"
-app.tokens_list = []
+app.tokens = []
+
+
+
+from starlette.authentication import (
+    AuthenticationBackend, AuthenticationError, SimpleUser, UnauthenticatedUser,
+    AuthCredentials
+)
+from starlette.middleware import Middleware
+from starlette.middleware.authentication import AuthenticationMiddleware
+from starlette.responses import PlainTextResponse
+from starlette.routing import Route
+import base64
+import binascii
+
+
+class BasicAuthBackend(AuthenticationBackend):
+    async def authenticate(self, request):
+        if "Authorization" not in request.headers:
+            return AuthenticationError('auto not in')
+
+        auth = request.headers["Authorization"]
+        try:
+            scheme, credentials = auth.split()
+            if scheme.lower() != 'basic':
+                return AuthenticationError('Invalid basic')
+            decoded = base64.b64decode(credentials).decode("ascii")
+        except (ValueError, UnicodeDecodeError, binascii.Error) as exc:
+            raise AuthenticationError('Invalid basic auth credentials')
+
+        username, _, password = decoded.partition(":")
+        if username == "admin" and password == "admin":
+        	return AuthCredentials(["authenticated"]), SimpleUser(username)
+
+@app.post("/login/")
+def create_cookie(user: str, password: str, response: Response):
+	if request.user.is_authenticated:
+   		session_token = sha256(bytes(f"{user}{password}{app.secret_key}")).hexdigest()
+		app.tokens+=session_token
+		response = RedirectResponse(url='/welcome')
+    		response.set_cookie(key="session_token", value=session_token)
+		response.status_code=302
+    		return response
+
+@app.get("/data/")
+def create_cookie(*, response: Response, session_token: str = Cookie(None)):
+    if session_token not in app.tokens :
+        raise HTTPException(status_code=403, detail="Unathorised")
+    response.set_cookie(key="session_token", value=session_token)
+'''
 security = HTTPBasic()
 
 
@@ -48,4 +97,4 @@ def login(
     response= requests.post('https://pikachupy.herokuapp.com/welcome', allow_redirects=False)
     
     return response
-
+'''
