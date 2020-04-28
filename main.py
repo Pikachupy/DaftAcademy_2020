822b62c2a933819ef4ce6cdbb8c8a9ce87102d26
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-29 01:06:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ async def shutdown():
 @app.get("/tracks")
 async def getgtracks():
     app.db_connection.row_factory = sqlite3.Row
-    data = app.db_connection.execute(''' SELECT name FROM tracks LIMIT 10 OFFSET 0 ORDER BY TrackId''').fetchall()
+    data = app.db_connection.execute('''SELECT name FROM tracks ORDER BY TrackId LIMIT 10 OFFSET 0 ''').fetchall()
     return data
     
 
