dd4047f0d76f961acf181a88f358153310ea752b
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-29 02:29:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ async def tracks_with_comp(composer_name: str):
     app.db_connection.row_factory = sqlite3.Row
     query = '''SELECT name FROM tracks WHERE composer = %s ORDER BY name'''
     param=(composer_name,)
-    data = app1.db_connection.execute(query,param).fetchall()
+    data = app.db_connection.execute(query,param).fetchall()
     return data
 
 
