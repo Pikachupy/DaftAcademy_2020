2fad54d17998b74ce2c2120b21b3385db91e4123
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-29 17:57:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ async def getgtracks():
 @app.get("/tracks/composers/")
 async def tracks_with_comp(composer_name: str):
     app.db_connection.row_factory = sqlite3.Row
-    data = app.db_connection.execute('''SELECT name FROM tracks WHERE composer = @composer_name ORDER BY name''').fetchall()
+    data = app.db_connection.execute('SELECT name FROM tracks WHERE composer =? ORDER BY name',composer_name).fetchall()
     return data
 
 
