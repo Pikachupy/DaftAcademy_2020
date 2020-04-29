31cb9ddd6956f29e86b98d2c711546c3190862e2
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-29 02:27:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-from fastapi import APIRouter
 
-router = APIRouter()
-
-
-@router.get("/")
-def read_items():
-    return [{"name": "Item Foo"}, {"name": "item Bar"}]
-'''
 import sqlite3
 from fastapi import FastAPI
 from fastapi import Depends, Cookie, HTTPException
@@ -23,24 +15,22 @@ async def startup():
 async def shutdown():
     app.db_connection.close()
 
-    
+'''   
 @app.get("/tracks")
 async def getgtracks():
     app.db_connection.row_factory = sqlite3.Row
-    data = app.db_connection.execute(SELECT * FROM tracks ORDER BY TrackId LIMIT 10 OFFSET 0 ).fetchall()
+    data = app.db_connection.execute('SELECT * FROM tracks ORDER BY TrackId LIMIT 10 OFFSET 0').fetchall()
     return data
     
-
+'''
 
 @app.get("/tracks/composers/")
-async def tracks_with_comp(composer_name):
-    try:
-        app.db_connection.row_factory = sqlite3.Row
-        query = SELECT name FROM tracks WHERE composer = %s ORDER BY name
-        param=(composer_name,)
-        data = app1.db_connection.execute(query,param).fetchall()
-        return data
-    except mysql.connector.Error as error:
-        print("parameterized query failed {}".format(error))
-'''
+async def tracks_with_comp(composer_name: str):
+    app.db_connection.row_factory = sqlite3.Row
+    query = '''SELECT name FROM tracks WHERE composer = %s ORDER BY name'''
+    param=(composer_name,)
+    data = app1.db_connection.execute(query,param).fetchall()
+    return data
+
+
         
