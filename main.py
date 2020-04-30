bbc38e01b1914c8e69dfca089bc4187b9f8683b8
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-30 02:16:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def tracks_with_comp(composer_name):
         data = app.db_connection.execute('SELECT name FROM tracks WHERE composer LIKE ? ORDER BY name',tup).fetchall()
         return data
     else:
-        return Response(text={"detail":{"error": composer_name}},status_code=404)
+        return Response(status_code=404, text={"detail":{"error": composer_name}})
 
        
         
