17284139f0a228b214c49c2eabe951f2ed8ce739
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-04-30 02:00:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def tracks_with_comp(composer_name):
         data = app.db_connection.execute('SELECT name FROM tracks WHERE composer LIKE ? ORDER BY name',tup).fetchall()
         return data
     else:
-        return  sqlite3.Error(status_code=404)
+        return Response(status_code=status.404)
 
        
         
