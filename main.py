74d22fb887ab61b65cd3681c04faa115214afb14
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-05-01 18:22:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -68,11 +68,9 @@ async def addalbum(album: Album):
 
 @app.put("/albums/{album_id}")
 async def albid(album_id: int, album:Album):
-    #cursor = app.db_connection.execute("UPDATE albums SET title = ?", (album.title))
-    return album_id
-'''
+    cursor = app.db_connection.execute("UPDATE albums SET title = ?", (album.title))
     app.db_connection.commit()
     app.db_connection.row_factory = sqlite3.Row
     item2 = app.db_connection.execute("SELECT title FROM albums WHERE artistid = ?",(album_id, )).fetchall()
     return album
-'''
+
