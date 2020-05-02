603e3e2b2dd3b2280a19f80b902a162891056baa
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-05-02 17:49:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -102,5 +102,6 @@ async def cust(customer_id: int, customer: Customer):
     app.db_connection.row_factory = lambda cursor, x: x[0]
     data2 = app.db_connection.execute('SELECT customerid FROM customers').fetchall()
     if not (customer_id in data2):
-        raise HTTPException(status_code=404,detail="error",)
+        item={"detail": "error"}
+        return JSONResponse(status_code=404, content=item)
 
