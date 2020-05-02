840b975a146814af1d4828b20b91abee2dc5e9b1
Pikachupy <64335310+Pikachupy@users.noreply.github.com>
2020-05-02 18:49:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,61 +9,23 @@ from starlette.responses import RedirectResponse
 from fastapi.responses import JSONResponse
 from typing import Set
 
-import jose
+
 import secrets
 from typing import Dict, Optional
+from typing import Dict
 
-from fastapi import Depends, FastAPI, Response, status, Request, HTTPException
-from fastapi.templating import Jinja2Templates
-from fastapi.security import APIKeyCookie, HTTPBasic, HTTPBasicCredentials
-from jose import jwt
+from fastapi import FastAPI, Request, Response, status
 from pydantic import BaseModel
-from starlette.responses import RedirectResponse
 
 
 class Patient(BaseModel):
     name: str
-    surname: str
+    surename: str
 
 
-class DaftAPI(FastAPI):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.counter: int = 0
-        self.storage: Dict[int, Patient] = {}
-        self.security = HTTPBasic(auto_error=False)
-        self.secret_key = "kluczyk"
-        self.API_KEY = "session"
-        self.cookie_sec = APIKeyCookie(name=self.API_KEY, auto_error=False)
-        self.templates = Jinja2Templates(directory="templates")
-
-
-app = DaftAPI()
-
-
-def is_logged(session: str = Depends(app.cookie_sec), silent: bool = False):
-    try:
-        payload = jwt.decode(session, app.secret_key)
-        return payload.get("magic_key")
-    except Exception:
-        pass
-
-    if silent:
-        return False
-
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-
-
-def authethicate(credentials: Optional[HTTPBasicCredentials] = Depends(app.security)):
-    if not credentials:
-        return False
-
-    correct_username = secrets.compare_digest(credentials.username, "trudnY")
-    correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
-
-    if not (correct_username and correct_password):
-        return False
-    return True
+app = FastAPI()
+app.counter: int = 0
+app.storage: Dict[int, Patient] = {}
 
 
 @app.get("/")
@@ -71,56 +33,23 @@ def read_root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
 
-@app.get("/welcome")
-def welcome(request: Request, is_logged: bool = Depends(is_logged)):
-    return app.templates.TemplateResponse(
-        "welcome.html", {"request": request, "user": "trudnY"}
-    )
-
-
-@app.post("/login")
-async def login_basic(auth: bool = Depends(authethicate)):
-    if not auth:
-        response = Response(headers={"WWW-Authenticate": "Basic"}, status_code=401)
-        return response
-
-    response = RedirectResponse(url="/welcome")
-    token = jwt.encode({"magic_key": True}, app.secret_key)
-    response.set_cookie("session", token)
-    return response
-
-
-@app.post("/logout")
-async def logout(is_logged: bool = Depends(is_logged)):
-    response = RedirectResponse(url="/")
-    response.delete_cookie("session")
-    return response
+@app.api_route(path="/method", methods=["GET", "POST", "DELETE", "PUT", "OPTIONS"])
+def read_request(request: Request):
+    return {"method": request.method}
 
 
 @app.post("/patient")
-def add_patient(patient: Patient, is_logged: bool = Depends(is_logged)):
+def show_data(patient: Patient):
+    resp = {"id": app.counter, "patient": patient}
     app.storage[app.counter] = patient
-    response = RedirectResponse(url=f"/patient/{app.counter}")
     app.counter += 1
-    return response
-
-
-@app.get("/patient")
-def show_patients(is_logged: bool = Depends(is_logged)):
-    return app.storage
+    return resp
 
 
 @app.get("/patient/{pk}")
-def show_patient(pk: int, is_logged: bool = Depends(is_logged)):
+def show_patient(pk: int):
     if pk in app.storage:
         return app.storage.get(pk)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.delete("/patient/{pk}")
-def delte_patient(pk: int, is_logged: bool = Depends(is_logged)):
-    if pk in app.storage:
-        del app.storage[pk]
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 app = FastAPI()
